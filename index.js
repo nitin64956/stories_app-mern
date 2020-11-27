b70cdf5f4218c01a43d456fbbdcaf0a233e6efed
nitin64956 <nitin64956@gmail.com>
2020-11-27 22:49:55 +0530

    mern api first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv'
+
+const app = express()
+
+dotenv.config()
+
+
+
+app.use(bodyParser.json({limit:"30mb",extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+
+
+app.use('/posts',postRoutes)
+
+app.get('/',(req,res) => {
+    res.send('Hello to memories API')
+})
+
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(() => {
+    app.listen(PORT,() => {
+        console.log(`Server running on port -> ${PORT}`)
+    })
+}).catch((err) => console.log(err.message))
+
+mongoose.set('useFindAndModify',false)
+
